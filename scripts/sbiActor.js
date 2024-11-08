66a301726b3732f6345db5ff4300e80ee286db73
Aioros <aioros83@gmail.com>
2024-11-08 10:22:10 -0500

    Fixed issue in save activities (corrected previous approach)

--- scripts/sbiActor.js
+++ scripts/sbiActor.js
@@ -1098,16 +1098,6 @@ export class sbiActor {
                     }
                 }
             }
-        } else if (game.system.version > "4" && activity == "save" && itemData.system.damage) {
-            let activityId = Object.values(itemData.system.activities).find(a => a.type == "save")._id;
-            const damageRoll = itemData.system.damage.parts[0][0].replace("@mod", "").replace("+", "").trim();
-            const damageType = itemData.system.damage.parts[0][1];
-            const baseDamage = {
-                number: damageRoll.split("d")[0],
-                denomination: damageRoll.split("d")[1],
-                types: [damageType]
-            }
-            sUtils.assignToObject(itemData, `system.activities.${activityId}.damage.parts`, [baseDamage]);
         }
 
         const versatilematch = sRegex.versatile.exec(description);
