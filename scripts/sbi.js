d9eec8584fde3c5e414fc2169916eaf8d92d1a35
Aioros <aioros83@gmail.com>
2025-02-28 10:08:11 -0500

    Small fixes for v13

--- scripts/sbi.js
+++ scripts/sbi.js
@@ -6,12 +6,14 @@ Hooks.on("init", registerSettings);
 
 Hooks.on("renderActorDirectory", (app, html, data) => {
     sbiUtils.log("Rendering SBI button");
+    html = html instanceof jQuery ? html.get(0) : html;
     const importButton = document.createElement("button");
     importButton.id = "sbi-main-button";
+    importButton.setAttribute("type", "button")
     importButton.innerHTML = `<i class="fas fa-file-import"></i>Import Statblock`;
     importButton.addEventListener("click", () => {
         sbiUtils.log("SBI button clicked");
         sbiWindow.renderWindow();
     });
-    html.get(0).querySelector(".directory-footer").appendChild(importButton);
+    html.querySelector(".directory-footer").appendChild(importButton);
 });
